d252fb4316ae34236eeb3c706e1585d14b37b974
cam96stanley <cam96stanley@gmail.com>
2024-10-11 02:52:44 -0400

    Added main div in html and created render function
    Created the main div to hold the array in data.js.
    Created the function to render the array from data.js
    onto the DOM and added classes for the items.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 import { menuArray } from "./data.js";
+const mainDiv = document.getElementById("main-div");
+
+function render() {
+  menuArray
+    .map(function (menuArray) {
+      mainDiv.innerHTML += `
+    <p class="emoji">${menuArray.emoji}</p>
+    <h2 class="menu-item-name">${menuArray.name}</h2>
+    <p class="ingredients">${menuArray.ingredients}</p>
+    <p class="price">${menuArray.price}</p>`;
+    })
+    .join("");
+}
+render();
